512033ef31f17d93f8e28a94ce8b031c2dc08d0b
Lucas Pereira <lpmelo10@gmail.com>
2023-01-10 18:43:43 -0300

    feat: creating api

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,14 @@
 <?php
 
+use App\Http\Controllers\HelloWorldController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('hello/{name}', function ($name) {
+    return 'hello ' . $name;
+});
+
+Route::post('hello-post/{name}', [HelloWorldController::class, 'hello']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
